c6bd4a3178549ce54f10a084f3d619e6bf32954f
William <gcphost@gmail.com>
2018-06-23 09:39:04 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# serverless-ssm-init
-Serverless AWS SSM Parameter Initiation 
+This plugin will seed your AWS SSM Paramter Store with the environment you have defined in your serverless config.
+
+More information about SSM environment variables: [Server Variables from AWS SSM Parameter Store](https://serverless.com/framework/docs/providers/aws/guide/variables/#reference-variables-using-the-ssm-parameter-store)
+
+# Installation
+```bash
+npm i serverless-ssm-init
+```
+
+# Configuration
+In your `serverless.yml`:
+
+## Define the plugin
+
+```yaml
+plugins:
+    - serverless-ssm-init
+```
+## Define the configuration
+```yaml
+custom:
+  ssm: 
+    secure: ['SECURE_VAR']
+    ignore: ['SOME_VAR']
+    stages: ['dev', 'staging', 'production']
+```
+
+## Define the ssm environment variables
+```yaml
+  environment:
+    SECURE_VAR: ${ssm:/${self:custom.stage}/SECURE_VAR~true}
+    SOME_VAR: ${ssm:/${self:custom.stage}/SOME_VAR}
+```
+
+# Usage
+Seed using your .env values for the defaults:
+```
+sls ssm-init
+```
+
+Use a string instead of .env values for the defaults:
+```
+sls ssm-init --ssm-default='default'
+```
+
+# Verify
+* Login to your AWS Console 
+* Services -> Systems Manager 
+* Parameter Store
+
+You should now see your environment parameters.
---
+++ index.js
@@ -0,0 +1,127 @@
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const {
+  safeLoad,
+} = require('js-yaml');
+const {
+  resolve,
+} = require('path');
+
+require('dotenv').config();
+
+class ServerlessPlugin {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
+
+    this.commands = {
+      'ssm-init': {
+        usage: 'Sync SSM environment variables on deploy.',
+        lifecycleEvents: [
+          'deploy',
+        ],
+        options: {
+          'ssm-default': {
+            usage: 'Specify the default SSM (default .env) ' +
+              '(e.g. "--ssm-default \'My Default\'" or "-ssm-default \'My Default\'")',
+            required: false,
+            shortcut: 'sd',
+          },
+        },
+      },
+    };
+
+    this.hooks = {
+      'ssm-init:deploy': this.handle.bind(this),
+    };
+  }
+
+  handle() {
+    const ssmDefault = this.options['ssm-default'];
+    let config = {};
+
+    try {
+      config = safeLoad(fs.readFileSync(resolve(process.cwd(), 'serverless.yml'), 'utf8'));
+    } catch (error) {
+      //
+    }
+
+    const environment = config.provider ? config.provider.environment : {};
+    const env = Object.keys(environment);
+    const stage = this.serverless.service.provider.stage;
+    let secure = [];
+    let ignore = [];
+    let stages;
+
+    const ssm = new AWS.SSM({
+      region: this.serverless.service.provider.region || process.env.AWS_REGION,
+    });
+
+    if (this.serverless.service.custom.ssm) {
+      if (this.serverless.service.custom.ssm.secure) {
+        secure = this.serverless.service.custom.ssm.secure;
+      }
+
+      if (this.serverless.service.custom.ssm.ignore) {
+        ignore = this.serverless.service.custom.ssm.ignore;
+      }
+
+      if (this.serverless.service.custom.ssm.stages) {
+        stages = this.serverless.service.custom.ssm.stages;
+      }
+    }
+
+    if (stages.length && !stages.includes(stage)) {
+      this.serverless.cli.log(`Stage '${stage}' is not enabled for ssm-init. Allowed stages: ${stages}.`);
+      return;
+    }
+
+    this.serverless.cli.log(`Setting SSM params for stage '${stage}'.`);
+
+    const putSSM = params => new Promise((resolve, reject) => {
+      ssm.putParameter(params, (error) => {
+        if (error) {
+          return reject(error);
+        }
+
+        return resolve();
+      });
+    });
+
+    const asyncForEach = async (array, callback) => {
+      for (let index = 0; index < array.length; index++) {
+        await callback(array[index], index, array)
+      }
+    };
+
+    asyncForEach(env, async (item) => {
+      if (ignore.includes(item) || !environment[item] || !environment[item].startsWith('${ssm:')) {
+        return;
+      }
+
+      const Value = ssmDefault || process.env[item] ? process.env[item] : null;
+
+      if (!Value) {
+        return this.serverless.cli.log(`Missing value for '${item}.'`);
+      }
+
+      this.serverless.cli.log(`Setting '${item}.'`);
+
+      try {
+        await putSSM({
+          Name: `/${stage}/${item}`,
+          Type: secure[item] ? 'SecureString' : 'String',
+          Value,
+          Description: item,
+          Overwrite: false,
+        });
+      } catch (error) {
+        this.serverless.cli.log(`${error.message} ${item}`);
+      }
+
+      return true;
+    });
+  }
+}
+
+module.exports = ServerlessPlugin;
